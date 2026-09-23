4171fc56fd23c0dadbefb70e5bc890ee9d7a5c4b
Malakai <gam3rsuknight@gmail.com>
2026-09-23 15:44:03 +1000

    fixes for requirements

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-number = 0
-numbers = []
-while number < 99:
-    if number not in numbers:
-        print("{:02d}, ".format(number), end="")
-        numbers.append(number)
-        numbers.append((((number % 10) * 10) + (number // 10)))
-    number += 1
-print("{:02d}".format(number))
-number += 1
+dig1 = 0
+dig2 = 0
+
+while dig1 < 10:
+    while dig2 < 10:
+        print("{}".format(dig1), end="")
+        print("{}, ".format(dig2), end="")
+        dig2 += 1
+    dig1 += 1
+    dig2 = dig1
+
+print("99")
+
