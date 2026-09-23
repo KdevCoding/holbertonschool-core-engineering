42cfe4f3bff7aa032a1fd9386c3e8a850821f595
Malakai <gam3rsuknight@gmail.com>
2026-09-23 15:09:17 +1000

    file for tasks

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
-alphabet = "abcdfghijklmnoprstuvwxyz"
-print(alphabet)
+char = 97
+chars = []
+while char < 123:
+    if char != 101 and char != 113:
+        chars.append(char)
+    char += 1
+
+print("".join(chr(letter) for letter in chars))
