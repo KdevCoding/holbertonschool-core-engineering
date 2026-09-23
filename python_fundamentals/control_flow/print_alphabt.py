94f0af89d0f50a7f45c047a64604b63ed0fed80d
Malakai <gam3rsuknight@gmail.com>
2026-09-23 15:15:38 +1000

    format fix

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
 
-char = 97
-chars = []
-while char < 123:
-    if char != 101 and char != 113:
-        chars.append(char)
-    char += 1
-
-print("".join(chr(letter) for letter in chars))
+print("{}".format("".join(chr(char)
+      for char in range(97, 123) if char != 101 and char != 113)))
