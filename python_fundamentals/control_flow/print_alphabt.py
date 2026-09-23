8b80e50c92ecd3dd85ee805d9a8fb75bdf088e75
Malakai <gam3rsuknight@gmail.com>
2026-09-23 15:21:17 +1000

    fix std out

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 print("{}".format("".join(chr(char)
-      for char in range(97, 123) if char != 101 and char != 113)))
+      for char in range(97, 123) if char != 101 and char != 113)), end="")
