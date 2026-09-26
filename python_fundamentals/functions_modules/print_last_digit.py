d85feddf0ead9dedab0d3369cafc6426b3d8a735
Malakai <gam3rsuknight@gmail.com>
2026-09-26 20:15:17 +1000

    file for tasks

---
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def print_last_digit(number):
+    if number < 0:
+        ld = -number % 10
+    else:
+        ld = number % 10
+    print("{}".format(ld))
+    return ld
